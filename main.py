013d47a76668bcaa61139470053fb27c10b40a22
Osayuware <72750417+Osayuware@users.noreply.github.com>
2021-01-18 18:36:59 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 while True:
-    
+    if input.sound_level() > 5 :
+        music.siren.play_until_done() 
+        light.set_all(color.rgb(255,0,0))
+
--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+while (true) {
+    if (input.soundLevel() > 5) {
+        music.siren.playUntilDone()
+        light.setAll(color.rgb(255, 0, 0))
+    }
+    
+}
